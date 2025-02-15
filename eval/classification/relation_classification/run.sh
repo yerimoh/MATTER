7ee41c35d160bb1dc9bbb5a0ac1c5e7a0dca739d
yerimOh <yerim0210@korea.ac.kr>
2025-02-15 17:04:22 +0900

    Update run.sh

--- eval/classification/relation_classification/run.sh
+++ eval/classification/relation_classification/run.sh
@@ -5,7 +5,7 @@ preds_save_dir=preds/
 cache_dir=../.cache/
 export CUDA_VISIBLE_DEVICES=1
 
-for model_name in PickyBPE; do
+for model_name in MATTER; do
     echo $model_name
-    python -u relation_classification2.py --model_name $model_name --model_save_dir $model_save_dir --preds_save_dir $preds_save_dir --cache_dir $cache_dir
+    python -u relation_classification.py --model_name $model_name --model_save_dir $model_save_dir --preds_save_dir $preds_save_dir --cache_dir $cache_dir
 done
